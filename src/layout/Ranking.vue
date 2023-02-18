c24c6ece292f324d3792a98a7647e0ee320530b7
GiovannyGarzon <giovankyandres@gmail.com>
2023-02-18 05:05:52 -0300

    Updates

--- src/layout/Ranking.vue
+++ src/layout/Ranking.vue
@@ -2,13 +2,18 @@
     <div class="container">
         <div class="social-media">
            <div class="social-media__item">
-
+                <div class="social-media__discord-box">
+                    Discord
+                </div>
+                <div class="social-media__facebook-box">
+                    Facebook
+                </div>
            </div>
            <div class="social-media__item">
-            
+                
             </div>
         </div>
-        
+
         <div class="ranking">
             RANKING
         </div>
