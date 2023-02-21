701c933329b33b208a00cf8a4d229d4a6ec43e99
GiovannyGarzon <giovankyandres@gmail.com>
2023-02-21 03:22:20 -0300

    Beta Home Page

--- src/layout/Hero.vue
+++ src/layout/Hero.vue
@@ -1,9 +1,16 @@
 <template>
-  <picture class="hero">
-    <div class="hero__item"></div>
-    <div class="hero__item"></div>
-    <div class="hero__item"></div>
-  </picture>
+  <div class="hero-container">
+    <picture class="hero">
+      <div class="hero__item"></div>
+      <div class="hero__item"></div>
+      <div class="hero__item"></div>
+    </picture>
+    <!-- <div class="button">
+      <input type="checkbox" id="slider">
+      <label for="slider" title="slider action" class="slider-action">
+      </label>
+    </div> -->
+  </div>
 </template>
 
 <script setup>
