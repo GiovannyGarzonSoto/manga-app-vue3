2fdf7ae52bf77a32e3bbd1f1de86eb6ce6fe6064
GiovannyGarzon <giovankyandres@gmail.com>
2023-05-15 08:52:31 -0400

    RankingMangas module updated

--- src/layout/Sidebar.vue
+++ src/layout/Sidebar.vue
@@ -21,45 +21,57 @@
                     <a class="ranking-box__tag tag">Ver Todo &#x276D;</a>
                 </div>
                 <div class="ranking-box__titles">
-                    <div class="ranking-box__item">
-                        <div class="ranking-box__image">
-
-                        </div>
+                    <div @mouseleave="removeClass" @mouseenter="setClass" ref="titles" v-for="manga in mangasByViews" class="ranking-box__item">
+                        <img class="ranking-box__image" :src="`${manga.images.cover}`">
                         <div class="ranking-box__info">
-                            <h4 class="ranking-box__name">Dandadan</h4>
-                            <h4 class="ranking-box__author">Tatsu yukinobu</h4>
-                            <h4 class="ranking-box__views"> <span class="ranking-box__logo-ranking"></span> 987.876</h4>
-                        </div>
-                    </div>
-
-                    <div class="ranking-box__item">
-                        <div class="ranking-box__image">
-
-                        </div>
-                        <div class="ranking-box__info">
-                            <h4 class="ranking-box__name">Undead Unlock</h4>
-                            <h4 class="ranking-box__author">Yoshifumi Tozuka</h4>
-                            <h4 class="ranking-box__views"> <span class="ranking-box__logo-ranking"></span> 908.125</h4>
-                        </div>
-                    </div>
-
-                    <div class="ranking-box__item">
-                        <div class="ranking-box__image">
-
-                        </div>
-                        <div class="ranking-box__info">
-                            <h4 class="ranking-box__name">Sakamoto Days</h4>
-                            <h4 class="ranking-box__author">Yuto Suzuki</h4>
-                            <h4 class="ranking-box__views"> <span class="ranking-box__logo-ranking"></span> 908.125</h4>
+                            <h4 class="ranking-box__name">{{ manga.title }}</h4>
+                            <h4 class="ranking-box__author">{{ manga.author.name }}</h4>
+                            <h4 class="ranking-box__views"> <span class="ranking-box__logo-ranking"></span>{{ manga.views }}</h4>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    
+    </div>    
 </template>
 
-<script setup>
-name: 'Sidebar'
+<script >
+import axios from '@axios'
+import { onMounted, ref } from 'vue'
+
+export default {
+  name: 'Sidebar',
+  setup() {
+    const mangasByViews = ref([])
+    const titles = ref(null)
+    
+    const getMangasByViews = async() => {
+        const {data} = await axios.get('/manga/views')
+        mangasByViews.value = data.data
+        console.log("🚀 ~ file: Sidebar.vue:72 ~ getMangasByVies ~ mangasByViews.value:", mangasByViews.value)
+    }
+
+    const setClass = (e) => {
+        e.target.childNodes[1].childNodes[0].classList.add('ranking-box__name-hover')
+    }
+
+    const removeClass = (e) => {
+        e.target.childNodes[1].childNodes[0].classList.remove('ranking-box__name-hover')
+    }
+
+    onMounted(() => {
+        getMangasByViews()
+    })
+   
+    return {
+        mangasByViews,
+        titles,
+        setClass,
+        removeClass
+    }
+  }
+}
+
 </script>
+
+<style src="../scss/layout/_sidebar.scss"></style>
