3db637fc3838b4730d2c0503a242cd1c154a40cd
GiovannyGarzon <giovankyandres@gmail.com>
2023-05-15 07:21:57 -0400

    Manga Cards fixrd styles

--- src/layout/Updates.vue
+++ src/layout/Updates.vue
@@ -5,122 +5,38 @@
       <a class="header__tag tag">Todo lo Reciente &#x276D;</a>
     </header>
 
-    <section class="titles" v-if="chapters.length>0">
-      <div class="title" v-for="(chapter, i) in chapters" :key="chapter.id">
-        <div class="title__image-box" ref="titleImage">
+    <section class="titles" v-if="chapters.length > 0">
+      <div @mouseleave="removeMangaMask" @mouseenter="setMangaMask" class="title" v-for="(chapter, i) in chapters" :key="chapter.id">
+        <div ref="box" class="title__image-box">
           <img class="title__image" :src="`${chapter.manga.images.cover}`" alt="">
-          <span class="title__name">{{ chapter.title }}</span>
+          <span class="title__name">{{ chapter.manga.title }}</span>
           <span class="title__author">{{ chapter.author.name }}</span>
         </div>
-        <div class="title__info">
+        <div ref="info" class="title__info">
           <span class="title__number">#{{ chapter.number }}</span>
           <span class="title__premiere">Hoy</span>
           <span class="title__chapter">Capitulo {{ chapter.number }}: {{ chapter.title }}</span>
         </div>
       </div>
-      <div class="title" v-for="(chapter, i) in chapters" :key="chapter.id">
-        <div class="title__image-box" ref="titleImage">
+      <div @mouseleave="removeMangaMask" @mouseenter="setMangaMask" class="title" v-for="(chapter, i) in chapters" :key="chapter.id">
+        <div ref="box" class="title__image-box">
           <img class="title__image" :src="`${chapter.manga.images.cover}`" alt="">
-          <span class="title__name">{{ chapter.title }}</span>
+          <span class="title__name">{{ chapter.manga.title }}</span>
           <span class="title__author">{{ chapter.author.name }}</span>
         </div>
-        <div class="title__info">
+        <div ref="info" class="title__info">
           <span class="title__number">#{{ chapter.number }}</span>
           <span class="title__premiere">Hoy</span>
           <span class="title__chapter">Capitulo {{ chapter.number }}: {{ chapter.title }}</span>
         </div>
       </div>
-      <div class="title" v-for="(chapter, i) in chapters" :key="chapter.id">
-        <div class="title__image-box" ref="titleImage">
+      <div @mouseleave="removeMangaMask" @mouseenter="setMangaMask" class="title" v-for="(chapter, i) in chapters" :key="chapter.id">
+        <div ref="box" class="title__image-box">
           <img class="title__image" :src="`${chapter.manga.images.cover}`" alt="">
-          <span class="title__name">{{ chapter.title }}</span>
+          <span class="title__name">{{ chapter.manga.title }}</span>
           <span class="title__author">{{ chapter.author.name }}</span>
         </div>
-        <div class="title__info">
-          <span class="title__number">#{{ chapter.number }}</span>
-          <span class="title__premiere">Hoy</span>
-          <span class="title__chapter">Capitulo {{ chapter.number }}: {{ chapter.title }}</span>
-        </div>
-      </div>
-      <div class="title" v-for="(chapter, i) in chapters" :key="chapter.id">
-        <div class="title__image-box" ref="titleImage">
-          <img class="title__image" :src="`${chapter.manga.images.cover}`" alt="">
-          <span class="title__name">{{ chapter.title }}</span>
-          <span class="title__author">{{ chapter.author.name }}</span>
-        </div>
-        <div class="title__info">
-          <span class="title__number">#{{ chapter.number }}</span>
-          <span class="title__premiere">Hoy</span>
-          <span class="title__chapter">Capitulo {{ chapter.number }}: {{ chapter.title }}</span>
-        </div>
-      </div>
-      <div class="title" v-for="(chapter, i) in chapters" :key="chapter.id">
-        <div class="title__image-box" ref="titleImage">
-          <img class="title__image" :src="`${chapter.manga.images.cover}`" alt="">
-          <span class="title__name">{{ chapter.title }}</span>
-          <span class="title__author">{{ chapter.author.name }}</span>
-        </div>
-        <div class="title__info">
-          <span class="title__number">#{{ chapter.number }}</span>
-          <span class="title__premiere">Hoy</span>
-          <span class="title__chapter">Capitulo {{ chapter.number }}: {{ chapter.title }}</span>
-        </div>
-      </div>
-      <div class="title" v-for="(chapter, i) in chapters" :key="chapter.id">
-        <div class="title__image-box" ref="titleImage">
-          <img class="title__image" :src="`${chapter.manga.images.cover}`" alt="">
-          <span class="title__name">{{ chapter.title }}</span>
-          <span class="title__author">{{ chapter.author.name }}</span>
-        </div>
-        <div class="title__info">
-          <span class="title__number">#{{ chapter.number }}</span>
-          <span class="title__premiere">Hoy</span>
-          <span class="title__chapter">Capitulo {{ chapter.number }}: {{ chapter.title }}</span>
-        </div>
-      </div>
-      <div class="title" v-for="(chapter, i) in chapters" :key="chapter.id">
-        <div class="title__image-box" ref="titleImage">
-          <img class="title__image" :src="`${chapter.manga.images.cover}`" alt="">
-          <span class="title__name">{{ chapter.title }}</span>
-          <span class="title__author">{{ chapter.author.name }}</span>
-        </div>
-        <div class="title__info">
-          <span class="title__number">#{{ chapter.number }}</span>
-          <span class="title__premiere">Hoy</span>
-          <span class="title__chapter">Capitulo {{ chapter.number }}: {{ chapter.title }}</span>
-        </div>
-      </div>
-      <div class="title" v-for="(chapter, i) in chapters" :key="chapter.id">
-        <div class="title__image-box" ref="titleImage">
-          <img class="title__image" :src="`${chapter.manga.images.cover}`" alt="">
-          <span class="title__name">{{ chapter.title }}</span>
-          <span class="title__author">{{ chapter.author.name }}</span>
-        </div>
-        <div class="title__info">
-          <span class="title__number">#{{ chapter.number }}</span>
-          <span class="title__premiere">Hoy</span>
-          <span class="title__chapter">Capitulo {{ chapter.number }}: {{ chapter.title }}</span>
-        </div>
-      </div>
-      <div class="title" v-for="(chapter, i) in chapters" :key="chapter.id">
-        <div class="title__image-box" ref="titleImage">
-          <img class="title__image" :src="`${chapter.manga.images.cover}`" alt="">
-          <span class="title__name">{{ chapter.title }}</span>
-          <span class="title__author">{{ chapter.author.name }}</span>
-        </div>
-        <div class="title__info">
-          <span class="title__number">#{{ chapter.number }}</span>
-          <span class="title__premiere">Hoy</span>
-          <span class="title__chapter">Capitulo {{ chapter.number }}: {{ chapter.title }}</span>
-        </div>
-      </div>
-      <div class="title" v-for="(chapter, i) in chapters" :key="chapter.id">
-        <div class="title__image-box" ref="titleImage">
-          <img class="title__image" :src="`${chapter.manga.images.cover}`" alt="">
-          <span class="title__name">{{ chapter.title }}</span>
-          <span class="title__author">{{ chapter.author.name }}</span>
-        </div>
-        <div class="title__info">
+        <div ref="info" class="title__info">
           <span class="title__number">#{{ chapter.number }}</span>
           <span class="title__premiere">Hoy</span>
           <span class="title__chapter">Capitulo {{ chapter.number }}: {{ chapter.title }}</span>
@@ -132,17 +48,26 @@
 
 <script >
 import axios from '@axios'
-import { computed, nextTick, onMounted, ref, watch } from 'vue'
+import { onMounted, ref } from 'vue'
 
 export default {
   name: 'Updates',
   setup() {
     const chapters = ref([])
+    const box = ref(null)
+    const info = ref(null)
 
-    const getChapters = async() => {
-      const {data} = await axios.get('/chapter') 
+    const getChapters = async () => {
+      const { data } = await axios.get('/chapter')
       chapters.value = data.data
-      console.log("🚀 ~ file: Updates.vue:159 ~ getChapters ~ chapters.value:", chapters.value)
+    }
+
+    const setMangaMask = async (e) => {
+      e.target.childNodes[1].classList.add('title__info-hover')
+    }
+
+    const removeMangaMask = async (e) => {
+      e.target.childNodes[1].classList.remove('title__info-hover')
     }
 
     onMounted(() => {
@@ -151,6 +76,10 @@ export default {
 
     return {
       chapters,
+      setMangaMask,
+      removeMangaMask,
+      box,
+      info
     }
   }
 }
